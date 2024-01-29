41467085039e3b29bfce5c75242236e5b14d8605
jorgeduje <jorgeduje4@gmail.com>
2024-01-29 12:00:31 -0300

    se agregan imagnes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,17 @@
 import "./Navbar.css"
 
+import pepito from "./images/images.png"
+
 export const Navbar = ()=>{
 
     return <nav>
+
+        {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_ZW-GtC__I5tt7QpyN8aWrEm7b8brelQWaCO1s6V8eGa4E9OP9lQM4pMSOnxvwTsiVXk&usqp=CAU" />  */}
+        {/* <img src={pepito} alt="" /> */}
+        <img src="/public/images.png" alt="" />
+        
+        <img src="https://res.cloudinary.com/dnqfh2chg/image/upload/v1706540172/images_rz1plz.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dnqfh2chg/image/upload/v1706540193/images_o3zvni.jpg" alt="" />
         <ul>
             <li className="bulletPoint">productos</li>
             <li>servicios</li>
