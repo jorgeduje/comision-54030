3bb8947345ee11d3ff2b40bf955b418386ca5fb3
jorgeduje <jorgeduje4@gmail.com>
2024-01-29 11:43:58 -0300

    se quita codigo innecesario

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import "./App.css"
-import { Navbar } from "./Navbar"
-import {Footer} from "./Footer"
+
 
 const App = ()=>{
 
   
   return (
     <div>
-
-      {/* USAR EL NAVBAR */}
-      {/* Navbar() */}
-      <Navbar />
-
-     {/* <h1 style={ {color: "steelblue", fontSize: "2rem"} }>Este es el titulo</h1> */}
-     <h1 className="title">Este es el titulo</h1>
-     <h2>un subtitulo </h2>
-     <h2>Algo mas </h2>
-
-     <Footer />
-     
+      <h1>Esta todo limpio</h1>
     </div>
   )
 }
